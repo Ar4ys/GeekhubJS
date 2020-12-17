430f06ccf16a459ef10126de396ed423e5c805f1
Ar4ys <artur.yurko1@gmail.com>
2020-12-17 19:58:49 +0200

    [hw3] Fix TS warning in first task

--- Hometask3/task1.ts
+++ Hometask3/task1.ts
@@ -4,20 +4,16 @@ type FinalExpression<T> = (initValues: T) => T
 type Callback<T> = (initValues: T) => any
 
 function fFor<T>(
-		init: Init<T> | T,
+		init: Init<T>,
 		condition: Condition<T>,
 		finalExpression: FinalExpression<T>,
 		callback: Callback<T>
 ): void {
-	let initValues: T
-	if (typeof init === "function")
-		initValues = init()
-	else
-		initValues = init
+	const initValues: T = init()
 
 	if (condition(initValues)) {
 		callback(initValues)
 		finalExpression(initValues)
-		fFor(initValues, condition, finalExpression, callback)
+		fFor(() => initValues, condition, finalExpression, callback)
 	}
 }
